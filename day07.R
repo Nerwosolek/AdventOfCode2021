a7f205220aa781b7b8dd807681e69da199205f74
Nerwosolek <subtraction_game@gieorgijewski.pl>
2021-12-07 13:17:46 +0100

    Day 7 (Part I & II)

--- day07.R
+++ day07.R
@@ -8,15 +8,34 @@ f <- scan("data/input07.txt", sep = ",")
 align <- function(positions, toPos){
   sum(abs(positions-toPos))
 }
+minPos <- 0
+minFuel <- Inf
 for (pos in 0:1986){
-  minPos <- 0
-  minFuel <- Inf
   fuel <- align(f,pos)
   if (fuel < minFuel){
     minPos <- pos
     minFuel <- fuel
   }
-  return (minPos)
 }
+print(minFuel)
 
+# Part II -----------------------------------------------------------------
+
+fuel_burnt <- function(change){
+  (change+1)*change/2
+}
+align2 <- function(positions, toPos){
+  
+  sum(fuel_burnt(abs(positions-toPos)))
+}
+minPos <- 0
+minFuel <- Inf
+for (pos in 0:1986){
+  fuel <- align2(f,pos)
+  if (fuel < minFuel){
+    minPos <- pos
+    minFuel <- fuel
+  }
+}
+print(minFuel)
 
